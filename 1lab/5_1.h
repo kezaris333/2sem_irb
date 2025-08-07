90bc67028bf9fddd9cdac3ff0d2a82bb8b5a7de0
kezaris333 <mina.mina.01102005@gmail.com>
2025-08-08 00:32:20 +0300

    Update 5_1.h

--- 1lab/5_1.h
+++ 1lab/5_1.h
@@ -1,46 +1,18 @@
-#include <iostream>
-#include <cstring>
-#include <C:\Users\Миночка\Desktop\codes\binomial_priority\priority_queue.h>
-
-class binary_heap : public priority_queue
+class priority_queue
 {
 
 public:
 
-        class Node_binaryheap {
-        public:
-            int priority;
-            char* value;
-        };
+    virtual ~priority_queue() noexcept = default;
 
-    size_t size;
-    Node_binaryheap *array = nullptr;
-    size_t capacity;
+    virtual void insert (int priority, char const *value) = 0;
 
+    virtual char const *remove_max() = 0;
 
-public:
+    virtual char const *obtain_max() = 0; //const char * - нельзя модифицировать значение под укзаателем
 
-    binary_heap(size_t size_in);
-    binary_heap(const binary_heap& other) ;
-    binary_heap& operator= (const binary_heap& other);
-    binary_heap(binary_heap&& other) noexcept;
-    binary_heap& operator= (binary_heap&& other) noexcept;
-    ~binary_heap() noexcept override;
+    virtual priority_queue *merge(priority_queue *with) = 0; //слияние с уничтожением
 
-public:
-
-
-    void sifting_from_top_to_bottom (int current);
-    void sifting_from_bottom_to_top (int current);
-
-    void insert (int prior,  const char * val) override;
-    void swap (Node_binaryheap& one, Node_binaryheap&two);
-    void printer ();
-
-    const char *remove_max() override;
-    const char* obtain_max() override;
-
-    priority_queue* merge(priority_queue* with) override;//слияние с уничтожением
-    priority_queue* meld(priority_queue const* with) const override; //слияние без униточжения изначальных объектов
+    virtual priority_queue *meld (priority_queue const *with)  = 0; //слияние без униточжения изначальных объектов
 
 };
